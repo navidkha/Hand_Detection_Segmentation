ad7426b78e01dd675f1b2aea6a158f39f587d5ed
navid <navid.khalili.edu@gmail.com>
2022-07-15 15:23:57 +0200

    Creating bounding boxes and confidence measurement from the output of the network

--- main.cpp
+++ main.cpp
@@ -1,76 +1,86 @@
+#include<opencv2/opencv.hpp>
+#include<opencv2/dnn.hpp>
 #include <iostream>
-#include <fstream>
-#include <opencv2/opencv.hpp>
-#include <opencv2/dnn.hpp>
-#include <opencv2/dnn/all_layers.hpp>
 
 using namespace std;
 using namespace cv;
-using namespace dnn;
+
+const size_t inWidth = 300;
+const size_t inHeight = 300;
+const float WHRatio = inWidth / (float)inHeight;
+const char* classNames[] = { "background","hand" };// According to the needs of this category
+
+int main() {
+
+    Mat frame = cv::imread("26.jpg");
+    Size frame_size = frame.size();
 
 
-int main(int, char**){
-    
-    string file_path = ""; //specify the file path
-    string class_name = "person"; //change to just string
-    ifstream ifs(string(file_path + "object_detection_classes_coco.txt").c_str());
-    string line;
-    
-    //read neural networks from the files
-    auto net = readNet("frozen_inference_graph.pb", "graph.pbtxt", "TensorFlow");
-    
+    String weights = "frozen_inference_graph.pb";
+    String prototxt = "graph.pbtxt";
+    dnn::Net net = cv::dnn::readNetFromTensorflow(weights, prototxt);
 
-    
-    // CAN ADD GPU
-    // net.setPreferableBackend(DNN_BACKEND_CUDA);
-    // net.setPreferableTarget(DNN_TARGET_CUDA);
-    
-    
-    // Set a min confidence score for the detections
-    float min_confidente_score = 0.5;
-    
-    
-        
-        // load image
-        Mat image = imread("26.jpg");
-        
-        int image_height = image.cols;
-        int image_width = image.rows;
-        
-        
-        auto start = getTickCount();
-        
-        //create a blob from the image
-        Mat blob = blobFromImage(image, 1.0, Size(300,300), Scalar(127.5, 127.5, 127.5), true, false);
-        // set the blob to be input of the neural network
+    Size cropSize;
+    if (frame_size.width / (float)frame_size.height > WHRatio)
+    {
+        cropSize = Size(static_cast<int>(frame_size.height * WHRatio),
+            frame_size.height);
+    }
+    else
+    {
+        cropSize = Size(frame_size.width,
+            static_cast<int>(frame_size.width / WHRatio));
+    }
 
-        net.setInput(blob);
-        
-        //forward pass of the blob
-        Mat output = net.forward();
-        cout<< output.size();
-        auto end = getTickCount();
-        
-        
-        // matrix with all the detections
-        Mat results(output.size[2], output.size[3], CV_32F, output.ptr<float>());
-        
-        //Run through all the predictions
-        for (int i = 0; i < results.rows; i++){
-            int class_id = int(results.at<float>(i, 1));
-            int confidence = results.at<float>(i, 2);
-            
-            // Check if the detection is over the min threshold and then draw BB
-            if (confidence > min_confidente_score){
-                int bboxX = int(results.at<float>(i, 3) * image.cols);
-                int bboxY = int(results.at<float>(i, 4) * image.rows);
-                int bboxWidth = int(results.at<float>(i, 5) * image.cols - bboxX);
-                int bboxHeight = int(results.at<float>(i, 6) * image.rows - bboxY);
-                rectangle(image, Point(bboxX, bboxY), Point(bboxX + bboxWidth, bboxY + bboxHeight), Scalar(0, 0, 255), 2);
-            }
+    Rect crop(Point((frame_size.width - cropSize.width) / 2,
+        (frame_size.height - cropSize.height) / 2),
+        cropSize);
+
+
+    cv::Mat blob = cv::dnn::blobFromImage(frame, 1. / 255, Size(300, 300));
+    //cout << "blob size: " << blob.size << endl;
+
+    net.setInput(blob);
+    Mat output = net.forward();
+    //cout << "output size: " << output.size << endl;
+
+    Mat detectionMat(output.size[2], output.size[3], CV_32F, output.ptr<float>());
+
+    frame = frame(crop);
+    float confidenceThreshold = 0.30;
+    for (int i = 0; i < detectionMat.rows; i++)
+    {
+        float confidence = detectionMat.at<float>(i, 2);
+
+        if (confidence > confidenceThreshold)
+        {
+            size_t objectClass = (size_t)(detectionMat.at<float>(i, 1));
+
+            int xLeftBottom = static_cast<int>(detectionMat.at<float>(i, 3) * frame.cols);
+            int yLeftBottom = static_cast<int>(detectionMat.at<float>(i, 4) * frame.rows);
+            int xRightTop = static_cast<int>(detectionMat.at<float>(i, 5) * frame.cols);
+            int yRightTop = static_cast<int>(detectionMat.at<float>(i, 6) * frame.rows);
+
+            ostringstream ss;
+            ss << confidence;
+            String conf(ss.str());
+
+            Rect object((int)xLeftBottom, (int)yLeftBottom,
+                (int)(xRightTop - xLeftBottom),
+                (int)(yRightTop - yLeftBottom));
+
+            rectangle(frame, object, Scalar(0, 255, 0), 2);
+            String label = String(classNames[objectClass]) + ": " + conf;
+            int baseLine = 0;
+            Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+            rectangle(frame, Rect(Point(xLeftBottom, yLeftBottom - labelSize.height),
+                Size(labelSize.width, labelSize.height + baseLine)),
+                Scalar(0, 255, 0), FILLED);
+            putText(frame, label, Point(xLeftBottom, yLeftBottom),
+                FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0));
         }
-        
-        imshow("image", image);
-    
+    }
+    imshow("image", frame);
+    waitKey(0);
     return 0;
 }
